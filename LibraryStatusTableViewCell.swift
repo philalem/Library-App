49be851d9373fadb30617804a12b2430c8249221
Phillip LeMaster <philalem21@gmail.com>
2018-06-06 18:53:10 -0400

    Saving UI

---
+++ LibraryStatusTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LibraryStatusTableViewCell.swift
+//  Library App
+//
+//  Created by Phillip LeMaster on 6/4/18.
+//  Copyright © 2018 Phillip LeMaster. All rights reserved.
+//
+
+import UIKit
+
+class LibraryStatusTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
